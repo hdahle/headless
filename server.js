2d7d14aee2bb5ba1b48d350d85fff834d90407d0
Håkon Dahle <hakon.dahle@gmail.com>
2020-12-29 16:59:13 +0100

    specific route for cb.js override

--- server.js
+++ server.js
@@ -27,12 +27,24 @@ if (!port) {
 // Let's serve some web pages
 console.log('Serving: ', folder);
 console.log('Port: ', port);
-console.log('Selfdestruct: ', selfdestruct);
+console.log('Selfdestruct: ', selfdestruct === undefined ? 'No' : 'Yes');
 
-app.use(express.static(folder));
-app.get("/", (req, res) => {
-  res.send('Server running');
+// This snippet replaces the existing cb.js file
+app.get("/js/cb.js", (req, res) => {
+  res.send('Chart.defaults.global.animation.onComplete = (e) => onCompleteCallback(e.chart); \
+    function onCompleteCallback(id) { \
+    let div = document.getElementById("pngPlaceHolder"); \
+    let img = document.createElement("IMG"); \
+    img.className = "fp-image"; \
+    img.id = id.canvas.id + "-png"; \
+    img.src = id.toBase64Image(); \
+    div.appendChild(img); \
+  }'
+  )
 });
+
+// All the regular web stuff is served here
+app.use(express.static(folder));
 
 let server = app.listen(port, () => {
   console.log('Server listening at: ', port);
